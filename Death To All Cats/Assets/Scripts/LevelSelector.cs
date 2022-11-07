05454e05cf3e5eb5a6266cba741c3fe19cfc6cbc
MalouOfTheDoom <mael.devpro@gmail.com>
2022-11-07 19:14:44 +0100

    Adds LevelSelector
    Also Adds TextMeshPro Assets.
    I didn't succeed ignoring it in .gitignore

---
+++ Death To All Cats/Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class LevelSelector : MonoBehaviour
+{
+    public int level;
+    public TextMeshProUGUI levelText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelText.text = level.ToString();
+    }
+
+    public void OpenScene()
+    {
+        SceneManager.LoadScene("Level " + level.ToString());
+    }
+}
