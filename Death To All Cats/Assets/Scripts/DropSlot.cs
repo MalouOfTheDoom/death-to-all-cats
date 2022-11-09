b12d0e9a6b64969846ae6727084800e46d3002a3
Yoan <yoan.roszak@etu.imt-nord-europe.fr>
2022-11-09 02:41:17 +0100

    fix-cartesmultiples

--- Death To All Cats/Assets/Scripts/DropSlot.cs
+++ Death To All Cats/Assets/Scripts/DropSlot.cs
@@ -9,9 +9,9 @@ public class DropSlot : MonoBehaviour, IDropHandler
     public void OnDrop(PointerEventData eventData)
     {
         GameObject dropped = eventData.pointerDrag;
+        // redondant mais ne fonctionne pas sans
         CardDragAndDrop cardDragAndDrop = dropped.GetComponent<CardDragAndDrop>();
         cardDragAndDrop.parentAfterDrag = transform;
-        cardDragAndDrop.GetComponent<ActionCardDisplay>().minimize();
-        // cardDragAndDrop.transform.localScale = new Vector3(5/4*0.01f,5/4*0.01f,5/4*0.01f);
+        gameObject.GetComponent<CharacterActions>().addActionCard(dropped);
     }
 }
